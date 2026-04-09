969e2e3f327f5bea8863630f08f66cd970ca7c25
Arvesh Gosine <arveshgosine@gmail.com>
2026-04-09 00:42:42 -0400

    #103 Deploy Postgresql server

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,7 @@
-SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
+import os
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SQLALCHEMY_DATABASE_URI",  
+    "sqlite:///temp-database.db"  
+)
 SECRET_KEY="secret key"
